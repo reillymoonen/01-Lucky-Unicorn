c5c902b26a206bb79a58fed46486fbc962a781a4
reillymoonen <135081213+reillymoonen@users.noreply.github.com>
2023-06-19 10:27:48 +1200

    Create 03_Instructions_v1.py

---
+++ 00_LU_Base_v_01.py
@@ -0,0 +1,35 @@
+
+
+# Functions go here...
+def yes_no(questions):
+    valid = False
+    while not valid:
+        responce = input("Have you played this game "  
+                         "before?").lower()
+
+        if responce == "yes" or responce == "y":
+                responce = "yes"
+                return responce
+
+        elif responce == "no" or responce == "n":
+                responce = "no"
+                return responce
+
+        else:
+            print("Please answer yes / no")
+
+def instructions():
+    print("**** How to Play ****")
+    print()
+    print("The rules of the game go here")
+    print()
+    return ""
+
+# Main Routine goes here...
+played_before = yes_no("Have you played the "
+                                "game before?")
+
+if played_before == "no":
+    instructions()
+
+print("Program continues")
