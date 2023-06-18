05818df5dbe5d92b82d2f15e5f179b6bdbeab9d6
reillymoonen <135081213+reillymoonen@users.noreply.github.com>
2023-06-19 10:57:35 +1200

    Update 04_how_much_v1.py

--- 04_how_much_v1.py
+++ 04_how_much_v1.py
@@ -3,18 +3,24 @@
 
 # Main routine goe here
 
-error = "Please enter an whole number beetween 1 and 10"
+error = "Please enter an whole number between 1 and 10\n"
 
 valid = False
 while not valid:
-    # ask the question
-    responce = int(input("How much would you "
-                   "like to play with?"))
-    # if the amount is too low / too high give
-    if 0 < responce <= 10:
-        print("You have asked to play "
-              "with ${}".format(responce))
+    try:
+        # ask the question
+        responce = int(input("How much would you "
+                       "like to play with? "))
+        # if the amount is too low / too high give
+        if 0 < responce <= 10:
+            print("You have asked to play "
+                  "with ${}".format(responce))
 
-    # output an error
-    else:
+        # output an error
+        else:
+            print(error)
+
+    except ValueError:
         print(error)
+
+# Print the
