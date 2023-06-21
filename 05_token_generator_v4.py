7e588691d474f997a5a39c5d8d9bc1a57cd890ff
reillymoonen <135081213+reillymoonen@users.noreply.github.com>
2023-06-21 13:17:23 +1200

    Update 05_token_generator_v4.py

--- 05_token_generator_v4.py
+++ 05_token_generator_v4.py
@@ -6,16 +6,18 @@ STARTING_BALANCE = 100
 
 balance = STARTING_BALANCE
 # Testing loop to generate 20 tokens
-for item in range(0,100):
-    chosen_num = random.choice(1, 100)
+for item in range(0,500):
+    chosen_num = random.randint(1, 100)
 
     # Adjust balance
     if 1 <= chosen_num <= 5:
         chosen = "unicorn"
         balance += 4
-    elif chosen == "donkey":
+    elif 6 <= chosen_num <= 36:
+        chosen = "donkey"
         balance -= 1
     else:
+        chosen = "horse / zebra"
         balance -= 0.5
 
 
