d804ea376384ce926cef712232c3f8e744e09887
reillymoonen <135081213+reillymoonen@users.noreply.github.com>
2023-06-21 12:46:28 +1200

    Update 00_LU_Base_v_01.py

--- 00_LU_Base_v_01.py
+++ 00_LU_Base_v_01.py
@@ -26,11 +26,36 @@ def instructions():
     return ""
 
 
+def num_check(question, low, high):
+    error = "Please enter an whole number between 1 and 10\n"
+
+    valid = False
+    while not valid:
+        try:
+            # ask the question
+            responce = int(input(question))
+            # if the amount is too low / too high give
+            if low < responce <= high:
+                return responce
+
+            # output an error
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
 # Main Routine goes here...
-played_before = yes_no("Have you played the "
-                                "game before?")
+played_before = yes_no("Have you played the game before? ")
 
 if played_before == "no":
     instructions()
 
-print("Program continues")
+print()
+
+# Ask user how much they want to play with...
+how_much = num_check("How much would you "
+                     "like to play with? ", 0, 10)
+
+print("You will be spending ${}".format(how_much))
