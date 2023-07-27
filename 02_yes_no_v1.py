4e8239428a6779550961fba9a885e62a3117b9d6
reillymoonen <135081213+reillymoonen@users.noreply.github.com>
2023-07-28 08:59:38 +1200

    Update

--- 02_yes_no_v1.py
+++ 02_yes_no_v1.py
@@ -1,26 +1,26 @@
-
 # Functions go here...
 def yes_no(questions):
     valid = False
     while not valid:
-        responce = input("Have you played this game "  
+        responce = input("Have you played this game "
                          "before?").lower()
 
         if responce == "yes" or responce == "y":
-                responce = "yes"
-                return responce
+            responce = "yes"
+            return responce
 
         elif responce == "no" or responce == "n":
-                responce = "no"
-                return responce
+            responce = "no"
+            return responce
 
         else:
             print("Please answer yes / no")
 
+
 # Main Routine goes here...
 
 show_instructions = yes_no("Have you played the "
-                                "game before?")
+                           "game before?")
 print("You choose {}".format(show_instructions))
 print()
 having_fun = yes_no("Are you having fun? ")
